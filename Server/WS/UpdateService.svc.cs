ad86fe723ecaaea2a6178cd2f80acc92596dcf96
DamienDijol <79765@supinfo.com>
2013-09-30 21:04:09 +0200

    Update UpdateService.svc.cs

--- Server/WS/UpdateService.svc.cs
+++ Server/WS/UpdateService.svc.cs
@@ -5,7 +5,6 @@ namespace Server.WS
     /// <summary>
     /// Permet de
     /// *récupérer la version du client.
-    /// todo: non peut-être rien de plus ici...
     /// </summary>
     public class UpdateService : IUpdateService
     {
